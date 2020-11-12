061dcc612a69f8fa76bee2d0bcc0a19797eed74b
Abhiraj Kale <abhirajkale1806@gmail.com>
2020-11-13 01:30:03 +0530

    backend-patch-1

--- routes/index.js
+++ routes/index.js
@@ -30,28 +30,11 @@ router.post('/validate', (req, res) =>{
   const third = parseInt(req.body.third); 
   const fourth = parseInt(req.body.fourth); 
   const avg = (first+second+third+fourth)/4;
-  console.log(first + " " + second + " " + third + " " + fourth);
-  console.log(first+second+third+fourth);
-  console.log(avg);
     var sql = `INSERT INTO user_info (mem1, mem2, mem3, mem4, avg) VALUES (?, ?, ?, ?, ?)`;
-    console.log(sql);
     con.query(sql,[er_first,er_second,er_third,er_fourth,avg],function (err, result) {
       if (err) throw err;
       console.log("1 record inserted");
     }); 
     res.end('<h1>Your Response has been recorded.</h1>')
-   /*
-    var id;
-    con.query("SELECT id FROM user_info WHERE mem1=? && mem2=? && mem3=? && mem4=? && avg=?",[er_first,er_second,er_third,er_fourth,avg], function (err, result, fields) {
-      if (err) throw err;
-      id = resul[0].id;
-    });
-    res.redirect('./form/'+id)
-  */
 })
-/*
-router.get('/form/:id', (req, res)=>{
-  res.render('index', {group:req.params.id})
-})
-*/
 module.exports = router;
