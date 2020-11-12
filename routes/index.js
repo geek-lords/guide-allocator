6819de9dc68a7225429a56052702cee3ba73f59e
Abhiraj Kale <abhirajkale1806@gmail.com>
2020-11-13 00:58:19 +0530

    Update

--- routes/index.js
+++ routes/index.js
@@ -25,17 +25,17 @@ router.post('/validate', (req, res) =>{
   const er_second = req.body.er_second;
   const er_third = req.body.er_third; 
   const er_fourth = req.body.er_fourth;
-  const first = req.body.first;
-  const second = req.body.second;
-  const third = req.body.third; 
-  const fourth = req.body.fourth; 
+  const first = parseInt(req.body.first);
+  const second = parseInt(req.body.second);
+  const third = parseInt(req.body.third); 
+  const fourth = parseInt(req.body.fourth); 
   const avg = (first+second+third+fourth)/4;
   console.log(first + " " + second + " " + third + " " + fourth);
   console.log(first+second+third+fourth);
   console.log(avg);
-    var sql = `INSERT INTO user_info (mem1, mem2, mem3, mem4, avg) VALUES (${er_first}, ${er_second}, ${er_third}, ${er_fourth}, ${avg})`;
+    var sql = `INSERT INTO user_info (mem1, mem2, mem3, mem4, avg) VALUES (?, ?, ?, ?, ?)`;
     console.log(sql);
-    con.query(sql, function (err, result) {
+    con.query(sql,[er_first,er_second,er_third,er_fourth,avg],function (err, result) {
       if (err) throw err;
       console.log("1 record inserted");
     }); 
