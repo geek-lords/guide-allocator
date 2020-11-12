427eb799df1a54b9322c7f02dffeec351635e3fc
Abhiraj Kale <abhirajkale1806@gmail.com>
2020-11-12 23:43:45 +0530

    update

--- routes/index.js
+++ routes/index.js
@@ -23,12 +23,12 @@ router.post('/validate', (req, res) =>{
   const third = req.body.third; 
   const fourth = req.body.fourth; 
   const avg = (first+second+third+fourth)/4;
-  res.end(first+second+third+fourth);
   var sql = `INSERT INTO user_info (mem1, mem2, mem3, mem4, avg) VALUES (${er_first}, ${er_second}, ${er_third}, ${er_fourth}, ${avg})`;
   con.query(sql, function (err, result) {
     if (err) throw err;
     console.log("1 record inserted");
   });
+  res.end(first+second+third+fourth);
 })
 
 module.exports = router;
