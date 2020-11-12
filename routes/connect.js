96b1705b7e6d9cfbb667cf9ea9e0f745f8d7e22a
Abhiraj Kale <abhirajkale1806@gmail.com>
2020-11-12 23:29:33 +0530

    update

--- routes/connect.js
+++ routes/connect.js
@@ -1,18 +1,37 @@
 function connect(){
 var mysql = require('mysql');
 
-var con = mysql.createConnection({
-  host: "us-cdbr-east-02.cleardb.com",
-  user: "bf3fd87e41d689",
-  password: "a00a933d"
-});
-
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected to Database!");
-});
-
-exports.con = con;
+var db_config = {
+    host: "us-cdbr-east-02.cleardb.com",
+    user: "bf3fd87e41d689",
+    password: "a00a933d",
+    database: 'heroku_30a612b411cf238'
+  };
+  
+  function handleDisconnect() {
+    con = mysql.createConnection(db_config); // Recreate the connection, since
+                                                    // the old one cannot be reused.
+  
+    con.connect(function(err) {              // The server is either down
+      if(err) {                                     // or restarting (takes a while sometimes).
+        console.log('error when connecting to db:', err);
+        setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+      }                                     // to avoid a hot loop, and to allow our node script to
+    });                                     // process asynchronous requests in the meantime.
+                                            // If you're also serving http, display a 503 error.
+                                            con.on('error', function(err) {
+      console.log('db error', err);
+      if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+        handleDisconnect();                         // lost due to either server restart, or a
+      } else {                                      // connnection idle timeout (the wait_timeout
+        throw err;                                  // server variable configures this)
+      }
+    });
+    
+  exports.con = con;
+  }
+  
+  handleDisconnect();
 }
 
 module.exports = {connect};
