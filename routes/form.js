4d7758426ed157cde21823055b5cc53939d7c509
Abhiraj Kale <abhirajkale1806@gmail.com>
2020-11-15 18:04:30 +0530

    backend-patch-4

--- routes/form.js
+++ routes/form.js
@@ -77,7 +77,7 @@ router.get('/submit/calc', (req,res)=>{
         con.query("UPDATE user_info SET `Assigned`=? WHERE id=?",[list[j],user_result[i].id],(err)=>{
           if (err) throw err;
         });
-        con.query("UPDATE guide_info SET `assigned`=? WHERE id=?",[list[j],guide_result[list[j]-1]],(error)=>{
+        con.query("UPDATE guide_info SET `assigned`=? WHERE id=?",guide_result[list[j]-1]].assgined+1,guide_result[list[j]-1]],(error)=>{
             if(err) throw error;
         })        
         break;
