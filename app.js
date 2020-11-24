fd28a237b1d9c68715fd01e0dcb6bfc3249e20a8
Abhiraj Kale <abhirajkale1806@gmail.com>
2020-11-24 14:10:23 +0530

    submit forms.ejs

--- app.js
+++ app.js
@@ -24,10 +24,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/form', formRouter);
 
-app.get('*', function(req, res){
-  res.status(404).send('<h1>Page not found</h1>');
-});
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
