429c563c44771bd5e308350cd5970c879902a19e
Abhiraj Kale <abhirajkale1806@gmail.com>
2020-11-24 14:08:40 +0530

    submit forms.ejs

--- routes/form.js
+++ routes/form.js
@@ -118,9 +118,4 @@ router.post('/submit/calc', (req,res)=>{
   }
   res.status(200).send("Success");
 })
-
-router.get('*', function(req, res){
-  res.status(404).send('<h1>Page not found</h1>');
-});
-
 module.exports = router;
