aa500ccb879898107100f138a1d56acd1901d428
Abhiraj Kale <abhirajkale1806@gmail.com>
2020-11-24 14:12:24 +0530

    submit forms.ejs

--- routes/index.js
+++ routes/index.js
@@ -106,9 +106,5 @@ router.post('/validate', (req, res) =>{
       })
     }
   })
-
 })
-router.get('*', function(req, res){
-  res.status(404).send('<h1>Page not found</h1>');
-});
 module.exports = router;
