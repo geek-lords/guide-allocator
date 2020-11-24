c5b23711bf2987b592f0dfa2afd5fe7081e56a77
Abhiraj Kale <abhirajkale1806@gmail.com>
2020-11-24 14:14:46 +0530

    submit forms.ejs

--- routes/index.js
+++ routes/index.js
@@ -16,11 +16,11 @@ const con = mysql.createConnection(db_config);
 router.get('/', function(req, res, next) {
   res.render('Home');
 });
-/*
+
 router.get('/:query',(req,res)=>{
   res.redirect('/');
 })
-*/
+
 router.post('/admin', (req, res)=>{
   const code = (req.body.code);
   if(!code || 0 === code.length || /^\s*$/.test(code))  
